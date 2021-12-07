7e3437d61d18a2b29881a6597b245aa79cfe8d5f
Pieter Bruyninckx <pieter.bruyninckx@verint.com>
2021-12-07 11:02:25 +0000

    Day 7 part 2
    The approach could definitely be reduced to O(n log n),
    but O(n^2), with n=1000, still seems to work fast enough.
    
    I do wonder whether there's a closed form solution though.

--- src/aoc/day07.clj
+++ src/aoc/day07.clj
@@ -16,13 +16,20 @@
          (map #(abs (- % median-data)))
          (reduce +))))
 
+(defn cost2 [p x]
+  (let [d (abs (- p x))]
+    (/ (* d (inc d)) 2)))
+
+(defn total-cost [data p]
+  (->> data
+       (map (partial cost2 p))
+       (reduce +)))
+
 (defn solve2 [data]
-  (let [mean-data (/ (reduce + data) (count data))
-        _ (println (count data))
-        _ (println mean-data)]
-    (->> data
-         (map #(abs (- % mean-data)))
-         (reduce +))))
+  (let [unique-data (into #{} data)]
+    (->> unique-data
+         (map (partial total-cost data))
+         (apply min))))
 
 
 (defn -main []
