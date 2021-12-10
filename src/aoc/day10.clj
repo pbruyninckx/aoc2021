368631d3f48ea3d3f881174daa1d8344c5b27bc2
Pieter Bruyninckx <pieter.bruyninckx@verint.com>
2021-12-10 12:31:23 +0000

    Day 10 part 2

--- src/aoc/day10.clj
+++ src/aoc/day10.clj
@@ -10,23 +10,38 @@
 
 (def close-map {\( \), \[ \], \{ \}, \< \> })
 
-(defn first-incorrect [line]
+(defn examine [line]
   (loop [expected []
          [c & rest-line :as line] line]
     (cond
-      (empty? line) nil
+      (empty? line) {:expected expected}
       (= (peek expected) c) (recur (pop expected) rest-line)
       (close-map c) (recur (conj expected (close-map c)) rest-line)
-      :else c)))
+      :else {:failing c})))
 
 (defn solve1 [lines]
   (->> lines
-       (map first-incorrect)
+       (map examine)
+       (map :failing)
        (filter identity)
        (map score-map)
        (reduce +)))
 
-(defn solve2 [lines])
+(def score-map2 {\) 1, \] 2, \} 3, \> 4})
+
+(defn solve2 [lines]
+  (->> lines
+       (map examine)
+       (map :expected)
+       (filter identity)
+       (map reverse)
+       (map #(reduce (fn [score c]
+                       (+ (* 5 score)
+                          (score-map2 c)))
+                     0
+                     %))
+       sort
+       ((fn [vals] (nth vals (quot (count vals) 2))))))
 
 (defn -main []
   (let [input (parse-input "resources/input10.txt")]
