ba22c787934acf7b090eb4650d8e506bca5d5b44
Pieter Bruyninckx <pbruyninckx@gmail.com>
2021-12-12 16:19:09 +0000

    Day 12 part 2
    An additional parameter was added to handle the extra option.
    
    Execution feels fairly slow though.

--- src/aoc/day12.clj
+++ src/aoc/day12.clj
@@ -19,25 +19,30 @@
   (Character/isLowerCase ^Character (first cave)))
 
 (defn get-paths
-  ([graph]
-   (get-paths graph #{"start"} '("start")))
-  ([graph small-visited [last-visited & _ :as path]]
+  ([part graph]
+   (get-paths graph #{"start"} '("start") (if (= part 1) true false)))
+  ([graph small-visited [last-visited & _ :as path] small-twice]
    (cond (= "end" last-visited) (list path)
          :else
-         (let [next-nodes (set/difference (graph last-visited) small-visited)]
+         (let [next-nodes (set/difference (graph last-visited)
+                                          (if small-twice small-visited #{})
+                                          #{"start"})]
            (if (empty? next-nodes)
              '()
              (apply concat
                     (for [node next-nodes]
                       (get-paths graph
                                  (if (small? node) (conj small-visited node) small-visited)
-                                 (conj path node)))))))))
+                                 (conj path node)
+                                 (or small-twice (small-visited node))))))))))
 
 (defn solve1 [graph]
-  (->> (get-paths graph)
+  (->> (get-paths 1 graph)
        count))
 
-(defn solve2 [graph])
+(defn solve2 [graph]
+  (->> (get-paths 2 graph)
+       count))
 
 (defn -main []
   (let [input (parse-input "resources/input12.txt")]
